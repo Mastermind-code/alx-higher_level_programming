264f982d638ee15c16ed3c617bb7c80678af4d5b
Adebowale Adam <adebowaleadam0@gmail.com>
2023-10-05 22:44:10 +0100

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -8,10 +8,10 @@
 #     b = 2
 #     print("{} + {} = {}".format(a, b, add(a, b)))
 
-if __name__ == "__main__":
-    """a function that imports from a module that adds the sum of 2 integers a and b"""
-    from add_0 import add
 
-    a = 1
-    b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+"""a function that imports from a module that adds the sum of 2 integers a and b"""
+from add_0 import add
+
+a = 1
+b = 2
+print(f"{a} + {b} = {add(a, b)}")
