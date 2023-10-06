ed3511593e41a1a26abbe3a0232daa888c799bfd
Adebowale Adam <adebowaleadam0@gmail.com>
2023-10-06 09:57:52 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    operators ={'+': add, '-': sub, '*':mul, '/': div}
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
     count = len(sys.argv) - 1
     if count < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
