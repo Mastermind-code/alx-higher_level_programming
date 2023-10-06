6a1f1bb6821a6648cd33c566e22f598885c36655
Adebowale Adam <adebowaleadam0@gmail.com>
2023-10-06 01:30:26 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,22 @@
 #!/bin/python3
 
-import sys
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-operators ={'+': add, '-': sub, '*':mul, '/': div}
-count = len(sys.argv) - 1
-if count < 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
-    exit(1)
+    operators ={'+': add, '-': sub, '*':mul, '/': div}
+    count = len(sys.argv) - 1
+    if count < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        exit(1)
 
-operator = None or sys.argv[2]
-if operator not in operators:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-else:
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    func = operators[operator](a, b)
-    print("{} + {} = {}".format(a, b, func))
-    exit(0)
+    operator = None or sys.argv[2]
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        func = operators[operator](a, b)
+        print("{} + {} = {}".format(a, b, func))
+        exit(0)
