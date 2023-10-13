4c99ea720bed4c4211fddab93108ce74e8b7f7cc
Adam Adebowale <Adebowaleadam0@gmail.com>
2023-10-13 14:13:24 +0000

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    while (len(tuple_a) <2) or (len(tuple_b) < 2):
+    while (len(tuple_a) < 2) or (len(tuple_b) < 2):
         if len(tuple_a) < 2:
             a = list(tuple_a)
             a.append(0)
-         if len(tuple_b) < 2:
+        if len(tuple_b) < 2:
             b = list(tuple_b)
             a.append(0)
 
