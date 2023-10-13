bdc8e431d493f5c86c68ae344b4b860bb2026306
Adam Adebowale <Adebowaleadam0@gmail.com>
2023-10-13 13:35:30 +0000

    2-replace_in_list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,4 +7,7 @@ def replace_in_list(my_list, idx, element):
       return my_list
     else:
       my_list[idx] = element 
+      return my_list
+
+# replace_in_list([1, 2, 3] / idx = 1 / element = 4)
       
