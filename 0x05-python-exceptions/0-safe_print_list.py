4c133ba553fbc5cfc241fb89a4d7e8e66bb34122
Adebowale Adam <adebowaleadam0@gmail.com>
2023-10-26 00:43:14 +0100

    All

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-            
+            print("{}".format(my_list[i]), end="")
+            counter += 1
         except IndexError:
-            return i   
-    return x
-
-my_list = [1, 2, 3, 4, 5]
-
-x = safe_print_list(my_list, 2)
+            pass
+    print("")
+    return counter
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
-    if isinstance(value, int):
+    try:
         print("{:d}".format(value))
-        return isinstance(value, int)
-    
+    except BaseException:
+        return False
+    else:
+        return True
