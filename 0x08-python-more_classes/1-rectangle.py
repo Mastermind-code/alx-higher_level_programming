09618866fa8085fa6fc374511ad142bb5e557bb2
Adebowale Adam <adebowaleadam0@gmail.com>
2023-11-02 01:03:52 +0100

    0x08-Python-More-Classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,28 +5,28 @@ Rectangle class
 
 
 class Rectangle:
-    def __init__(self, width=0, height=0) -> None:
+    """class defining a rectangle"""
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-    @property  
+    @property
     def width(self):
-        return self.__width
-        
+        return (self.__width)
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("Width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
-        return self.__width
-    
-        
+        return (self.__width)
+
     @property
     def height(self):
-        return self.__height
-    
+        return (self.__height)
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -34,4 +34,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        return self.__height
+        return (self.__height)
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,14 +5,15 @@ Rectangle class
 
 
 class Rectangle:
-    def __init__(self, width=0, height=0) -> None:
+    """class defining a rectangle"""
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        return self.__width
-    
+        return (self.__width)
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -20,11 +21,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        return self.__width
-    
+        return (self.__width)
+
     @property
     def height(self):
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
@@ -33,16 +34,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        return self.__height
-    
-    def area(self):
-        area = self.__height * self.__width
-        return area
-    
+        return (self.__height)
 
-    def perimeter(self)
-        if self.__height == 0 or self.__width == 0:
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
             return 0
-        else:
-            perimeter = 2*(self.__height * self.__width)
-            return perimeter
+        return 2 * (self.__width + self.__height)
